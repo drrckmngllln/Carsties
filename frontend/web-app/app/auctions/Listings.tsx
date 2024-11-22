39cd4e321a339b858094f714cfc419c8db18cb3f
Derrick Manglallan <127472849+drrckmngllln@users.noreply.github.com>
2024-11-22 16:57:53 +0800

    problem with the pagination

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import AuctionCard from './AuctionCard';
+import { Auction, PagedResult } from '@/types';
+import AppPagination from '../components/AppPagination';
+import { getData } from '../actions/auctionActions';
+import Filters from './Filters';
+import { useParamsStore } from '../hooks/useParamsStore';
+import { useShallow } from 'zustand/shallow';
+import qs from 'query-string';
+export default function Listings() {
+    const [data, setData] = useState<PagedResult<Auction>>();
+    const params = useParamsStore(useShallow(state => ({
+        pageNumber: state.pageNumber,
+        pageSize: state.pageSize,
+        searchTerm: state.searchTerm
+    })));
+
+    const setParams = useParamsStore(useShallow(state => state.setParams));
+    const url = qs.stringifyUrl({url: '', query: params})
+
+    const setPageNumber = (pageNumber: number) => {
+        setParams({pageNumber})
+    }
+
+    useEffect(() => {
+        getData(url).then(data => {
+            setData(data);
+        })
+    }, [url])
+
+    if (!data) return <h3>Loading...</h3>
+
+    return (
+        <>
+            <Filters />
+            <div className='grid grid-cols-4 gap-6'>
+                {data?.results.map(auction => (
+                    <AuctionCard auction={auction} key={auction.id} />
+                ))}
+            </div>
+            <div className='flex justify-center mt-4'>
+                <AppPagination pageChanged={setPageNumber} 
+                    currentPage={params.pageNumber} pageCount={data!.pageCount} />
+            </div>
+        </>
+    )
+}
